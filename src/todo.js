bce5cbcf671dfcb9585acfc429c9f0b4adf05ff8
Patrick Weygand <patrick.weygand@gmail.com>
2019-01-31 12:21:45 -0800

    add a basic toggle

--- src/todo.js
+++ src/todo.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
 
 export default class ToDo extends Component {
+  state = {
+    isEditing: false,
+    text: ''
+  }
+
+  onEdit = e =>
+    this.setState({isEditing: true})
+
   render () {
     const {
       text
     } = this.props
 
+    const {
+      isEditing
+    } = this.state
+
     return <div className='todo-item'>
-      <span className='todo-content'>{text}</span>
-      <button type='button'>edit</button>
-      <button type='button'>delete</button>
+      {isEditing
+        ? <span className='todo-item_editfield'>
+          <input
+            className='todo-item__input'
+            value={text}
+          />
+          <button
+            type='button'
+          >
+            Save
+          </button>
+          <button
+            type='button'
+          >
+            Cancel
+          </button>
+        </span>
+        : <span>
+          <span className='todo-content'>{text}</span>
+          <button type='button' onClick={this.onEdit}>edit</button>
+          <button type='button'>delete</button>
+        </span>
+      }
     </div>
   }
 }
